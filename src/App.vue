951a909ba0e4f8316ee3f584fc35716b7f9b447c
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-10 14:01:17 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+</script>
+
+<template>
+  <h1>Ciao</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
