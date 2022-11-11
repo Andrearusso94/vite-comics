c261383d38f8164001082ca4807962230f0d9dfc
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-11 15:49:49 +0100

    end ex

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -25,17 +25,13 @@ export default {
                     href: '#',
                 },
                 {
-                    text: 'Video',
+                    text: 'Videos',
                     href: '#',
                 },
                 {
                     text: 'News',
                     href: '#',
                 },
-                {
-                    text: 'Shop',
-                    href: '#',
-                }
             ],
             shop: [
                 {
@@ -122,41 +118,43 @@ export default {
 </script>
 <template>
     <footer id="site-footer">
-        <div class="container_link container">
-            <div class="row">
-                <div class="col-2">
-                    <h3>DC COMICS</h3>
-                    <ul>
-                        <li>
-                            <a href="">Characters</a>
-                        </li>
-                    </ul>
-                    <h3 class="my-2">Shop</h3>
-                    <ul class="my-2">
-                        <li>
-                            <a href="">Shop DC</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2">
-                    <h3>DC</h3>
-                    <ul>
-                        <li>
-                            <a href="">terms of use</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-2">
-                    <h3>Sites</h3>
-                    <ul>
-                        <li>
-                            <a class="secondary" href="">terms of use</a>
-                        </li>
+        <div class="container_link">
+            <div class="container footer-top py-5">
+                <div class="row">
+                    <div class="col-2">
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li v-for="item in linkMenu">
+                                <a :href="item.href">{{ item.text }}</a>
+                            </li>
+                        </ul>
+                        <h3 class="my-2">Shop</h3>
+                        <ul class="my-2">
+                            <li v-for="item in shop">
+                                <a :href="item.href">{{ item.text }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <h3>DC</h3>
+                        <ul>
+                            <li v-for="item in dcMenu">
+                                <a :href="item.href">{{ item.text }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-2">
+                        <h3>Sites</h3>
+                        <ul>
+                            <li v-for="item in sites">
+                                <a :href="item.href">{{ item.text }}</a>
+                            </li>
 
-                    </ul>
-                </div>
-                <div class="col-6">
-                    <img src="../assets/img/dc-logo-bg.png" alt="">
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        <img src="../assets/img/dc-logo-bg.png" alt="">
+                    </div>
                 </div>
             </div>
         </div>
@@ -164,15 +162,16 @@ export default {
         <div class="container_social">
             <div class="container d-flex  justify-content-between">
                 <div class="shop">
-                    <button class="my-5">Now</button>
+                    <button class="my-5">Sign-Up Now!</button>
                 </div>
 
                 <div class="social my-5 d-flex">
                     <h3 class="mx-2">FOLLOW US</h3>
                     <img class="mx-2" src="../assets/img/footer-facebook.png" alt="">
-                    <img class="mx-2" src="../assets/img/footer-facebook.png" alt="">
-                    <img class="mx-2" src="../assets/img/footer-facebook.png" alt="">
-                    <img class="mx-2" src="../assets/img/footer-facebook.png" alt="">
+                    <img class="mx-2" src="../assets/img/footer-twitter.png" alt="">
+                    <img class="mx-2" src="../assets/img/footer-youtube.png" alt="">
+                    <img class="mx-2" src="../assets/img/footer-pinterest.png" alt="">
+                    <img class="mx-2" src="../assets/img/footer-periscope.png" alt="">
                 </div>
             </div>>
 
