e08e050065b85aa35fcdbfb7f03bcf34e8143201
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-12 09:49:43 +0100

    add ShopComic.vue

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -1,7 +1,11 @@
 <script>
 import comics from '../data/comics.js'
+import ShopComic from './ShopComic.vue'
 export default {
     name: 'Shop',
+    components: {
+        ShopComic
+    },
     data() {
         return {
             fumetti: comics
@@ -14,10 +18,8 @@ export default {
     <div class="container_primary p-5">
         <div class="container">
             <div class="row">
-                <div class="col-2 my-2" v-for="comics in fumetti">
-                    <img :src="comics.thumb" :alt="comics.type">
-                    <h6 class="my-3">{{ comics.series }}</h6>
-                </div>
+                <ShopComic v-for="comics in fumetti" :thumb="comics.thumb" :series="comics.series" />
+
                 <div class="btn-container container my-2">
                     <button class="btn btn-primary">LOAD MORE</button>
                 </div>
@@ -31,18 +33,6 @@ export default {
 
 .container_primary {
     background-color: $dark;
-
-
-    h6 {
-        color: $secondary_color;
-        text-transform: uppercase;
-        font-size: 0.65rem;
-    }
-
-    img {
-        width: 150px;
-        height: 150px;
-    }
 }
 
 .btn-container {
---
+++ src/components/ShopComic.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: {
+        thumb: String,
+        series: String,
+    }
+
+}
+</script>
+
+<template>
+
+    <div class="col-2 my-2">
+        <img :src="thumb" :alt="type">
+        <h6 class="my-3">{{ series }}</h6>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/variables.scss' as *;
+
+
+
+
+h6 {
+    color: $secondary_color;
+    text-transform: uppercase;
+    font-size: 0.65rem;
+}
+
+img {
+    width: 150px;
+    height: 150px;
+}
+</style>
